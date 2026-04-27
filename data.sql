d5417d02d7ba2cf778b9a826ca3d36fe1f48744f
tranducminh26 <133018709+tranducminh26@users.noreply.github.com>
2026-04-27 05:05:36 +0000

    Thêm data.sql

---
+++ data.sql
@@ -0,0 +1,13 @@
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE todos (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT,
+    title VARCHAR(255) NOT NULL,
+    completed BOOLEAN DEFAULT FALSE,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
